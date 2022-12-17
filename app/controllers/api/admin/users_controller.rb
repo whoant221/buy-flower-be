aa464da66e63fe044e43984ed466a2538565d445
whoant <vovanhoangtuan4.2@gmail.com>
2022-12-17 22:55:20 +0700

    Route update user

--- app/controllers/api/admin/users_controller.rb
+++ app/controllers/api/admin/users_controller.rb
@@ -11,9 +11,22 @@ module Api
 
       def create
         AuthenticateService::Register.new(params: user_params).create
+        render json: {}, status: :created
+      end
+
+      def update
+        UserService::Client.new(user: user).update(user_params)
+        render json: {}, status: :ok
       end
 
       private
+
+      def user
+        @user ||= User.find_by_id(params[:id])
+        raise ActiveRecord::RecordNotFound, I18n.t('services.user_service.not_exists') unless @user
+
+        @user
+      end
 
       def user_params
         params.permit(:email, :password, :phone_number, :password_confirmation, :name, :address)
